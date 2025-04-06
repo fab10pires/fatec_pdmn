9070bee5a4a9040ab8b03c48abce578471d077fd
Fabio Pires <fabiopiresferreira@gmail.com>
2025-04-06 13:48:26 -0300

    learn{ap05-react}: encerra a apostila

--- componentes-props/src/App.jsx
+++ componentes-props/src/App.jsx
@@ -1,21 +1,61 @@
+import Pedido from "./Pedido"
+import Cartao from "./Cartao"
+import Feedback from "./Feedback"
 const App = () => {
+  const textoOK = "Já chegou"
+  const textoNOK = "Ainda não chegou"
+  const funcaoOK = () => alert("Agradecemos o feedback")
+  const funcaoNOK = () => alert("Verificaremos o que aconteceu")
+  const componenteFeedback = (
+    <Feedback 
+      textoOK={textoOK}
+      textoNOK={textoNOK}
+      funcaoOK={funcaoOK}
+      funcaoNOK={funcaoNOK}
+    />
+  )
   return (
     <div className="container border mt-2">
       <div className="row">
+
         <div className="col-sm-12 col-lg-6 col-xxl-3">
-          <p>caixa 1</p>
+          <Cartao cabecalho="22/03/2024">
+            <Pedido
+              icone="headset"
+              titulo="Headset"
+              descricao="Headset Bluetooth"/> 
+              {componenteFeedback}
+          </Cartao>         
         </div>
 
         <div className="col-sm-12 col-lg-6 col-xxl-3">
-          <p>caixa 2</p>
+          <Cartao cabecalho="22/04/2024">
+            <Pedido
+                icone="gamepad"
+                titulo="Gamepad"
+                descricao="Gamepad Nintendo"/>
+                {componenteFeedback}
+          </Cartao>          
         </div>
 
         <div className="col-sm-12 col-lg-6 col-xxl-3">
-          <p>caixa 3</p>
+        <Cartao cabecalho="22/01/2024">
+          <Pedido
+            icone="hippo"
+            titulo="Hipopótamo"
+            descricao="Hipopótamo Fêmea"/>
+            {componenteFeedback}
+          </Cartao>          
         </div>
 
         <div className="col-sm-12 col-lg-6 col-xxl-3">
-          <p>caixa 4</p>
+          <Cartao cabecalho="22/03/2024">
+            <Pedido
+              icone="snowman"
+              titulo="Boneco de neve"
+              descricao="Boneco de neve balançando"/>
+              {componenteFeedback}
+          </Cartao>          
         </div>
 
 
--- componentes-props/src/Feedback.jsx
+++ componentes-props/src/Feedback.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Feedback = ({ textoOK, funcaoOK, textoNOK, funcaoNOK}) => {
   return (
     <div className="d-flex justify-content-evenly mt-2">
